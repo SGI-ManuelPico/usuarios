0766e9474f2972aac0fda968174d677146a718b3
Manuelpd21 <desarrollo.software3@sgiltda.com>
2024-11-07 15:34:52 -0500

    .

--- db.py
+++ db.py
@@ -1,13 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# Define the database credentials
+HOST = 'localhost'
+USER = 'root'
+PASSWORD = '12345678'
+DATABASE = 'test2'
 
-DATABASE_URL = "mysql+mysqlconnector://root:12345678@localhost/test"
+# Update the DATABASE_URL with PyMySQL
+DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
 
+# Create the engine and session
 engine = create_engine(DATABASE_URL)
-
-
 Session = sessionmaker(bind=engine)
 
+# Function to get a new session
 def get_session():
     return Session()
